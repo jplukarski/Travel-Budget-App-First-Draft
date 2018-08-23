aba37d7b7c2c16315d8065b346455d69ad6c7842
JPlukarski <jwplukarski@gmail.com>
2018-08-22 21:24:37 -0500

    firebase working, city and nights spent added

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-$("#test").on("click", function () {
-    console.log("oof");
+var submitButton = $("#submit");
+var cityInput = $("#new-city");
+var database = firebase.database();
+var nightsInput = $("#nights-stayed");
+
+submitButton.on("click", function () {
+
+    var newCity = cityInput.val();
+    var nightsStayed = nightsInput.val();
+    database.ref().push({
+        newCity,
+        nightsStayed,
+    });
+
+});
+
+database.ref().on("child_added", function (snapshot) {
+    var data = snapshot.val();
+
+
+
+
+    $("#data-dump").append(
+        `<tr>
+        <td>${data.newCity}</td>
+        <td>${data.nightsStayed}</td>
+    <tr>`
+    )
 });
