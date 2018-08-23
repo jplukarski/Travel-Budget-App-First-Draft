4fcd84b07d6d9a1c2c54f357c01e312a7243eccb
JPlukarski <jwplukarski@gmail.com>
2018-08-22 21:44:08 -0500

    added amount spent

--- script.js
+++ script.js
@@ -2,21 +2,24 @@ var submitButton = $("#submit");
 var cityInput = $("#new-city");
 var database = firebase.database();
 var nightsInput = $("#nights-stayed");
+var amountInput = $("#amount-spent");
 
 submitButton.on("click", function () {
 
     var newCity = cityInput.val();
     var nightsStayed = nightsInput.val();
+    var amountSpent = amountInput.val();
     database.ref().push({
         newCity,
         nightsStayed,
+        amountSpent,
     });
 
 });
 
 database.ref().on("child_added", function (snapshot) {
     var data = snapshot.val();
-
+    var moneySign = "$"
 
 
 
@@ -24,6 +27,7 @@ database.ref().on("child_added", function (snapshot) {
         `<tr>
         <td>${data.newCity}</td>
         <td>${data.nightsStayed}</td>
+        <td>${moneySign + data.amountSpent}</td>
     <tr>`
     )
 });
