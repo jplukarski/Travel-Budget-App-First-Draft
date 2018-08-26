f884ca9b2670606d7e65bd11f2f6842c7936d889
JPlukarski <jwplukarski@gmail.com>
2018-08-26 15:28:03 -0500

    added a bunch of stuff

--- script.js
+++ script.js
@@ -4,30 +4,39 @@ var database = firebase.database();
 var nightsInput = $("#nights-stayed");
 var amountInput = $("#amount-spent");
 
+
+
+
+
 submitButton.on("click", function () {
 
     var newCity = cityInput.val();
     var nightsStayed = nightsInput.val();
     var amountSpent = amountInput.val();
+    function getselectedCurrency() {
+        var newCurrency = $("#current").val();
+        currencyInput = newCurrency
+    };
+    getselectedCurrency();
+
     database.ref().push({
         newCity,
         nightsStayed,
         amountSpent,
+        currencyInput,
     });
 
 });
 
 database.ref().on("child_added", function (snapshot) {
     var data = snapshot.val();
-    var moneySign = "$"
-
-
 
     $("#data-dump").append(
         `<tr>
         <td>${data.newCity}</td>
         <td>${data.nightsStayed}</td>
-        <td>${moneySign + data.amountSpent}</td>
+        <td>${data.amountSpent}</td>
+        <td>${data.currencyInput}</td>
     <tr>`
     )
 });
