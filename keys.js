d5305082b70e05fea01de1887240055ecc3f39d0
JPlukarski <jwplukarski@gmail.com>
2018-09-01 16:18:49 -0500

    connected the api using ajax

---
+++ keys.js
@@ -0,0 +1,3 @@
+exports.fixer = {
+    api_key: process.env.fixer_api
+}
--- script.js
+++ script.js
@@ -4,9 +4,20 @@ var database = firebase.database();
 var nightsInput = $("#nights-stayed");
 var amountInput = $("#amount-spent");
 var newCurrency = $("#current");
-
 // var mysql = require("mysql");
 
+// var keys = require("./keys");
+// var fixerAPI = keys.fixer.api_key
+// console.log("Fixer API Key" + process.env.fixer_api)
+
+var queryURL = "http://data.fixer.io/api/latest?access_key=0ed6f6268b1ac17ca8a1695ff2c8c153";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+})
 
 submitButton.on("click", function () {
 
