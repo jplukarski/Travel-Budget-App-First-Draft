c198330d228a645126414bb1fec347c37cf221d0
JPlukarski <jwplukarski@gmail.com>
2018-09-02 10:56:13 -0500

    messed around

--- script.js
+++ script.js
@@ -4,47 +4,68 @@ var database = firebase.database();
 var nightsInput = $("#nights-stayed");
 var amountInput = $("#amount-spent");
 var newCurrency = $("#current");
-// var mysql = require("mysql");
 
+// var mysql = require("mysql");
 // var keys = require("./keys");
 // var fixerAPI = keys.fixer.api_key
 // console.log("Fixer API Key" + process.env.fixer_api)
 
-var queryURL = "http://data.fixer.io/api/latest?access_key=0ed6f6268b1ac17ca8a1695ff2c8c153";
+/////////////////////////////////////////////////////////////////////////
+/////////       API Call       //////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
 
+var queryURL = "http://data.fixer.io/api/latest?access_key=0ed6f6268b1ac17ca8a1695ff2c8c153";
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function (response) {
-    console.log(response);
-})
 
+    var xyz = "USD"
+    console.log(response.rates[xyz]);
+});
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+
+
+/////////////////////////////////////////////////////////////////////////
+/////////  Pushes new entry to Firebase  ////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
 submitButton.on("click", function () {
-
     var newCity = cityInput.val();
     var nightsStayed = nightsInput.val();
     var amountSpent = amountInput.val();
     var currencyInput = newCurrency.val();
-
-
     database.ref().push({
         newCity,
         nightsStayed,
         amountSpent,
         currencyInput,
     });
-
 });
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
 
+
+/////////////////////////////////////////////////////////////////////////
+////////      Displaying data from Firebase     ///////////////// ///////
+/////////////////////////////////////////////////////////////////////////
 
 var transactions = [];
+var totalinEuros = [];
+
 database.ref().on("child_added", function (snapshot) {
     var data = snapshot.val();
     var spent = parseInt(data.amountSpent)
     var cash = data.currencyInput
 
     transactions.push(
-        spent
+        {
+            amountSpent: spent,
+            currencyUsed: cash
+        }
     );
 
     $("#data-dump").append(
@@ -55,27 +76,35 @@ database.ref().on("child_added", function (snapshot) {
         <td class="currency-type">${data.currencyInput}</td>
     </tr>`
     );
-
-
-
     function myFunction(item) {
         function getSum(total, num) {
             return total + num;
         };
+
+
         outputTotal = transactions.reduce(getSum)
-        console.log("Hm... " + outputTotal);
+        console.log("Output total: " + JSON.stringify(outputTotal));
         $("#total-spent").html(outputTotal);
     };
-
-
-
-
-
-
     myFunction();
 
-
-
-
 });
+
+function addToEuroTotalArray() {
+    for (i = 0; i < transactions.length; i++) {
+        if (transactions[i].currencyUsed === "EUR") {
+            totalinEuros.push(transactions[i].amountSpent);
+        }
+    }
+};
+
+
+console.log(totalinEuros);
+console.log(transactions);
+
+addToEuroTotalArray();
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
 
